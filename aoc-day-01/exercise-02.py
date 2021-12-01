443f96435fd4d84c37dcf3bcf5ef9677bb77c461
Kamil Wisniewski <kamil.m.wis@gmail.com>
2021-12-01 21:31:41 +0100

    Advent of code 2021 - Day 01

--- aoc-day-01/exercise-02.py
+++ aoc-day-01/exercise-02.py
@@ -3,8 +3,8 @@ with open('input.txt') as f:
 
 increases = 0
 
-for i in range(1, len(lines), 1):
-    if int(lines[i]) > int(lines[i - 1]):
+for i in range(3, len(lines), 1):
+    if int(lines[i]) > int(lines[i - 3]):
         increases += 1
 
 print(increases)
