8390181cccc9711707e895bcfa25833260447ea3
Kamil Wisniewski <kamil.m.wis@gmail.com>
2021-12-07 21:06:25 +0100

    Advent of code 2021 - Day 06

---
+++ aoc-day-06/exercise-02.py
@@ -0,0 +1,29 @@
+with open('input.txt') as f:
+    numbers = f.read().split(",")
+
+memory = {}
+
+
+def calculate(value, days, mem):
+    if value >= days:
+        return 0
+    else:
+        if mem.get(str(value) + "," + str(days)) is not None:
+            return mem[str(value) + "," + str(days)]
+        else:
+            basic_outcome = 1 + (days - value - 1) // 7
+            children_days = []
+            for i in range(basic_outcome):
+                children_days.append(days - value - 1 - (i * 7))
+            outcome = 0
+            outcome += basic_outcome
+            for day in children_days:
+                outcome += calculate(8, day, mem)
+            mem[str(value) + "," + str(days)] = outcome
+            return outcome
+
+
+answer = 0
+for n in numbers:
+    answer += calculate(int(n), 256, memory) + 1
+print(answer)
